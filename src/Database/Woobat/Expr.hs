0bf85d695f8623f19b833f76a0f65a5aa1ce39f5
Olle Fredriksson <fredriksson.olle@gmail.com>
2020-10-04 21:14:55 +0200

    Capitalise array_length

--- src/Database/Woobat/Expr.hs
+++ src/Database/Woobat/Expr.hs
@@ -246,7 +246,7 @@ overlap :: Expr s [a] -> Expr s [a] -> Expr s Bool
 overlap = unsafeBinaryOperator "&&"
 
 arrayLength :: Expr s [a] -> Expr s Int
-arrayLength (Expr e) = Expr $ "array_length(" <> e <> ", 1)"
+arrayLength (Expr e) = Expr $ "ARRAY_LENGTH(" <> e <> ", 1)"
 
 instance (NonNestedArray a, DatabaseType a) => DatabaseType [a] where
   typeName = typeName @a <> "[]"
